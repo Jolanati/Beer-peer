4645f633ca20d391641cda23f25eff3472d6a954
Jolanati <jolanta.stutane@gmail.com>
2026-05-05 22:55:13 +0300

    -1 Problem, 1 still pending

--- find_fft_cell.py
+++
@@ -1,9 +0,0 @@
-import json
-nb = json.load(open(r'c:\Users\jolanta.stutane\Desktop\RSU_AI\DL_Final\wine-dine\wine-dine.ipynb', encoding='utf-8'))
-for i, c in enumerate(nb['cells']):
-    src = ''.join(c.get('source', []))
-    if 'food_flavor_table.json' in src and 'WEIGHTS.parent' in src:
-        cid = c.get('id', '')
-        print(f'Cell {i+1}, id={cid}')
-        print(src[:800])
-        print()
